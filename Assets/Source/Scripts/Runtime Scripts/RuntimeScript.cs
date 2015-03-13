788159bdabb6fb87e0f9abf9a18f3d2deda022c1
jorgp2 <jorgperez2@gmail.com>
2015-03-12 20:19:03 -0500

    Probably made a mess of the script parser by using enumerators.

--- Assets/Source/Scripts/Runtime Scripts/RuntimeScript.cs
+++ Assets/Source/Scripts/Runtime Scripts/RuntimeScript.cs
@@ -3,12 +3,13 @@ using System.Collections;
 using System.IO;
 using System.Threading;
 
+
 public class RuntimeScript : MonoBehaviour{
 
 	Thread workerThread;
-	public string[] RawLines;
+	
 
-	public byte[] PackedScript; 
+	public ushort[] PackedScript; 
 
 	// Use this for initialization
 	void Start () {
@@ -22,20 +23,109 @@ public class RuntimeScript : MonoBehaviour{
 
 	public void InitilaizeScript(string FileName)
 	{
-		StreamReader Reader = new StreamReader(Application.dataPath+"\\" + FileName);
-		string All = Reader.ReadToEnd ();
-		RawLines = All.Split ((char) 13);
-		workerThread = new Thread ( new ThreadStart(PackScript));
-		workerThread.Start ();
+        PackScriptHelper MB = new PackScriptHelper();
+		workerThread = new Thread ( MB.PackScript);
+		workerThread.Start (FileName);
 	}
 
-	public void PackScript()
-	{
-
-	}
+	
 }
 
-public struct ScriptLookUpTable
+public class PackScriptHelper
 {
-	byte[] Instructions = {0};
+    public CodeSegements Seg;
+    
+    public void PackScript(object FileName)
+	{
+        StreamReader Reader = new StreamReader(Application.dataPath + "\\" + FileName);
+        Seg = new CodeSegements( Reader.ReadToEnd().Split(new char[] { ';'}));
+        IEnumerator SegEnum = Seg.GetEnumerator();
+        ProcessHeader(SegEnum.Current as string);
+
+
+	}
+
+    
+
+    void ProcessHeader(string header)
+    {
+        string[] xxx = header.Split(new char {':' });
+
+        int i = 1;
+
+
+    }
+
 }
+
+public class CodeSegements : IEnumerable
+{
+    public string[] Segments;
+
+    public CodeSegements(string[] segments)
+    {
+        Segments = new string[segments.Length];
+        for (int i = 0; i < segments.Length; i++)
+        {
+            Segments[i] = segments[i];
+        }
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return (IEnumerator)GetEnumerator();
+    }
+
+    public CodeEnum GetEnumerator()
+    {
+        return new CodeEnum(Segments);
+    }
+}
+
+public class CodeEnum : IEnumerator
+{
+    public string[] Segments;
+
+    int position = -1;
+
+    public CodeEnum(string[] list)
+    {
+        Segments = list;
+    }
+
+    public bool MoveNext()
+    {
+        position++;
+        return (position < Segments.Length);
+    }
+
+    public void Reset()
+    {
+        position = -1;
+    }
+
+    object IEnumerator.Current
+    {
+        get
+        {
+            return Current;
+        }
+    }
+
+    public string Current
+    {
+        get
+        {
+            //try
+            //{
+                return Segments[position];
+            //}
+            //catch ( ArrayIndexOutOfRangeException)
+            //{
+            //    throw new  InvalidOperationException();
+            //}
+        }
+    }
+}
+
+
